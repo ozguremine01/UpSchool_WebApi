a9162ebaf8ff59c94955cc16e6e9b2ae2af020ef
ozguremine <ozguremine01@gmail.com>
2022-12-20 13:16:08 +0300

    Add project files.

---
+++ UpSchool_WebApi/Dal/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UpSchool_WebApi.Dal.Entities;
+
+namespace UpSchool_WebApi.Dal
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-H7B28ES;database=DBCoreUpSchoolApi; integrated security=true");
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ UpSchool_WebApi/Dal/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace UpSchool_WebApi.Dal.Entities
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+    }
+}
